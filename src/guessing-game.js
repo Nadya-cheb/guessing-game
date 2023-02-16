a8c1d02e632721884a047142395c45915cb89e82
Nadya-cheb <nadya21cheb@gmail.com>
2023-02-16 09:32:15 +0300

    files: add code js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;          
     }
 
     guess() {
-
+        //предположение
+        this.res = this.min + Math.ceil((this.max - this.min)/2);
+        return this.res;
     }
 
     lower() {
-
+        //загаданное число меньше, чем предположение
+        this.max =  this.res;
     }
 
     greater() {
-
+        //загаданное число больше, чем предположение
+        this.min =  this.res;       
     }
 }
 
